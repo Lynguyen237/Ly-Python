287448fefb04576f327419161a174e57c92ebe78
Ly Nguyen (Personal) <lynguyen237@gmail.com>
2020-06-29 00:58:53 -0700

    Create a basic addition function using user input

---
+++ Practice_function_addition.py
@@ -0,0 +1,9 @@
+def three_adder(x):
+  var = x+3
+  # print (var)
+  return (var)
+
+value = three_adder (10)
+x = int(raw_input("give me a number: "))
+print three_adder(x)
+print three_adder(int(raw_input("Give another number: ")))
