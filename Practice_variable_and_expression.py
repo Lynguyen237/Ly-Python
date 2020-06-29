a40e86b1e2142bedab517f030d3dcbc388a3df51
Ly Nguyen (Personal) <lynguyen237@gmail.com>
2020-06-29 00:32:53 -0700

    Practice using variables and expressions

---
+++ Practice_variable_and_expression.py
@@ -0,0 +1,55 @@
+my_name = "balloonicorn"
+your_name = "Engineer"
+print (my_name == your_name)
+print (my_name != your_name)
+
+2+5
+print 2+5
+addition = 2 + 9
+print (addition)
+print type(addition)
+
+#exercise
+a = "Hello, world"
+message = "Howdy, folks!"
+my_num = 406
+i = 40
+print a
+print message
+print my_num
+print i
+print (9 == 10)
+print ("Hello" == "hello")
+print i + my_num
+print my_num - i
+
+L = "Larry"
+G = "Gina"
+E = "Erika"
+B = "Buster"
+print L, G, E, B
+length_of_balloonicorn = len("balloonicorn")
+print length_of_balloonicorn
+are_names_equal = ("Balloonicorn" == "baLLOONicorn")
+print are_names_equal
+
+#final exercise
+family_member1 = "Lai"
+family_member2 = "Thanh"
+family_member3 = "Cong"
+family_member4 = "Nga"
+print family_member1
+print len (family_member1)
+print family_member2
+print len (family_member2)
+print family_member3
+print len (family_member3)
+print family_member4
+print len (family_member4)
+new_member = raw_input ("Enter the name of a new family member: ")
+print new_member
+
+
+my_message = raw_input ("Write a message to yourself: ")
+print "Message to self: " + my_message
+
